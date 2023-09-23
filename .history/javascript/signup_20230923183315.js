c005083325b305a4f3b9336d09b950f403f7c670
OmarEssameldinMousa <124374760+OmarEssameldinMousa@users.noreply.github.com>
2023-09-23 18:41:25 +0300

    create two php files config and signup

---
+++ .history/javascript/signup_20230923183315.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".signup form"),
+    continueBtn = form.querySelector(".button input");
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+}
+
+continueBtn.onclick = () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/signup.php", true);
+    xhr.onload = () => {
+
+    }
+    xhr.send()
+}
