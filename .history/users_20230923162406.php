746e6512cc37dca90a8225be25e3477a9b1676d1
OmarEssameldinMousa <124374760+OmarEssameldinMousa@users.noreply.github.com>
2023-09-23 16:24:28 +0300

    starting working with php and ajax environment

---
+++ .history/users_20230923162406.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Realtime Chat App</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="users">
+            <header>
+                <div class="content">
+                    <img src="images/124374760.jpg" alt="">
+                    <div class="details">
+                        <span>Omar essam</span>
+                        <p>Active now</p>
+                    </div>
+                </div>
+                <a href="" class="Logout">Logout</a>
+            </header>
+            <div class="search">
+                <span class="text">Select an user to start Chat</span>
+                <input type="text" placeholder="Enter name to search...">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+            <div class="users-list">
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+                <a href="">
+                    <div class="content">
+                        <img src="images/124374760.jpg" alt="">
+                        <div class="details">
+                            <span>Omar essam</span>
+                            <p>This is test message</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+
+            </div>
+        </section>
+    </div>
+    <script src="javascript/users.js"></script>
+
+</body>
+
+</html>
